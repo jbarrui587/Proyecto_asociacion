8cb936362c1ef9420133096818b0fa38ab3b7e60
joseybarranco <joseyenred@hotmail.com>
2026-03-12 18:41:48 +0100

    creación de componente footer.jsx y se añaden fuente e imagenes

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import './bootstrap/bootstrap.min.css'
 import './bootstrap/bootstrap.min.js'
 import Nav from './components/nav.jsx'
 import Header from './components/header.jsx'
+import Footer from './components/footer.jsx'
 
 function App() {
   
-  const [users,setUsers] = useState([])
+  const [setUsers] = useState([])
   useEffect(() => {
     fetch('http://localhost:5000/api/users')
       .then(res => res.json())
@@ -22,29 +22,12 @@ function App() {
       <Header></Header>
 
       <Nav></Nav>
-      
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <main>
         
-          {users.map((user) =>(
-            <p key={user.id}>
-              {user.name}
-            </p>
-          ))}
+      </main>
+      <Footer></Footer>
       
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
---
+++ frontend-react/src/components/footer.jsx
@@ -0,0 +1,24 @@
+
+
+function Footer(){
+    return(
+        <footer>
+            <div>
+                <p>@ Copyright | Asociación de agua Natural Abderitana</p>
+                
+            </div>
+            <div>
+
+            </div>
+            <div>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>Tik Tok</li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
